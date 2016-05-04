06597a5f8a0dd847a501ae76621934cd04e5ef85
Emmanuel Szép <manuszep@gmail.com>
2016-05-04 14:56:55 +0200

    Declare service form as a service to gain access to authorization layer. Make the user field role dependent for service form so only users with sufficient role may choose the author of a service.

--- src/AppBundle/Form/ServiceType.php
+++ src/AppBundle/Form/ServiceType.php
@@ -14,10 +14,17 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use AppBundle\Entity\Service;
-use AppBundle\Form\Type\FieldsetType;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 
 class ServiceType extends AbstractType
 {
+    private $authorizationChecker;
+
+    public function __construct(AuthorizationChecker $authorizationChecker)
+    {
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
@@ -25,58 +32,55 @@ class ServiceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         // TODO: Search for a better way
+        $serivce = new Service();
 
-        $builder->add('mainFieldset', FieldsetType::class, [
-            'legend' => 'Test Fieldset!',
-            'fields' => function(FormBuilderInterface $builder) {
-                $serivce = new Service();
+        $types = array_flip($serivce->getTypes());
+        $domains = array_flip($serivce->getDomains());
 
-                $types = array_flip($serivce->getTypes());
-                $domains = array_flip($serivce->getDomains());
+        if ($this->authorizationChecker->isGranted('ROLE_EDITOR')) {
+            $builder->add('user', EntityType::class, array(
+                'class' => 'AppBundle:User',
+                'choice_label' => 'username',
+                'placeholder' => 'Auteur',
+                'label' => 'Auteur'
+            ));
+        }
 
-                $builder
-                    ->add('user', EntityType::class, array(
-                        'class' => 'AppBundle:User',
-                        'choice_label' => 'username',
-                        'placeholder' => 'Auteur',
-                        'label' => 'Auteur'
-                    ))
-                    ->add('title', TextType::class, array(
-                        'label' => 'Titre'
-                    ))
-                    ->add('body', TextareaType::class, array(
-                        'label' => 'Contenu'
-                    ))
-                    ->add('type', ChoiceType::class, array(
-                        'choices'  => $types,
-                        'label' => 'Type de service'
-                    ))
-                    ->add('domain', ChoiceType::class, array(
-                        'choices'  => $domains,
-                        'label' => 'Domaine'
-                    ))
-                    ->add('category', EntityType::class, array(
-                        'class' => 'AppBundle:Category',
-                        'choice_label' => 'select_label',
-                        'placeholder' => 'Catégorie',
-                        'label' => 'Catégorie'
-                    ))
-                    ->add('promote', CheckboxType::class, array(
-                        'label'    => 'Mettre en évidence ?',
-                        'required' => false,
-                    ))
-                    ->add('picture', FileType::class, array(
-                        'label' => 'Image'
-                    ))
-                    ->add('expires_at', DateType::class, array(
-                        'widget' => 'single_text',
-                        'required' => false,
-                        'label' => 'Date d\'expiration'
-                    ));
-            }
-        ])
-            ->add('Enregistrer', SubmitType::class, array(
-                'attr' => array('class' => 'form-action-main'),
+        $builder
+            ->add('title', TextType::class, array(
+                'label' => 'Titre'
+            ))
+            ->add('body', TextareaType::class, array(
+                'label' => 'Contenu'
+            ))
+            ->add('type', ChoiceType::class, array(
+                'choices'  => $types,
+                'label' => 'Type de service'
+            ))
+            ->add('domain', ChoiceType::class, array(
+                'choices'  => $domains,
+                'label' => 'Domaine'
+            ))
+            ->add('category', EntityType::class, array(
+                'class' => 'AppBundle:Category',
+                'choice_label' => 'select_label',
+                'placeholder' => 'Catégorie',
+                'label' => 'Catégorie'
+            ))
+            ->add('promote', CheckboxType::class, array(
+                'label'    => 'Mettre en évidence ?',
+                'required' => false,
+            ))
+            ->add('picture', FileType::class, array(
+                'label' => 'Image'
+            ))
+            ->add('expires_at', DateType::class, array(
+                'widget' => 'single_text',
+                'required' => false,
+                'label' => 'Date d\'expiration'
+            ))
+            ->add('save', SubmitType::class, array(
+                'attr' => array('class' => 'main'),
             ));
     }
     
