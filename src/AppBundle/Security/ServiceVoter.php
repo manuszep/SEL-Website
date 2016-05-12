cf6093c95dfb717bc984ba9e139a213dfa10b104
Emmanuel Szép <manuszep@gmail.com>
2016-05-12 14:27:14 +0200

    Add voter to handle service edit permissions

---
+++ src/AppBundle/Security/ServiceVoter.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace AppBundle\Security;
+
+use AppBundle\Entity\Service;
+use AppBundle\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
+
+class ServiceVoter extends Voter
+{
+    // these strings are just invented: you can use anything
+    const EDIT = 'edit';
+
+    private $decisionManager;
+
+    public function __construct(AccessDecisionManagerInterface $decisionManager)
+    {
+        $this->decisionManager = $decisionManager;
+    }
+
+    protected function supports($attribute, $subject)
+    {
+        // if the attribute isn't one we support, return false
+        if (!in_array($attribute, array(self::EDIT))) {
+            return false;
+        }
+
+        // only vote on Service objects inside this voter
+        if (!$subject instanceof Service) {
+            return false;
+        }
+
+        echo "supports ok";
+        return true;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            // the user must be logged in; if not, deny access
+            return false;
+        }
+
+        if ($this->decisionManager->decide($token, array('ROLE_EDITOR'))) {
+            return true;
+        }
+
+        echo "not role editor";
+
+        // you know $subject is a Service object, thanks to supports
+        /** @var Service $service */
+        $service = $subject;
+
+        switch($attribute) {
+            case self::EDIT:
+                echo "case edit";
+                return $this->canEdit($service, $user);
+        }
+
+        throw new \LogicException('This code should not be reached!');
+    }
+
+    private function canEdit(Service $service, User $user)
+    {
+        // this assumes that the data object has a getOwner() method
+        // to get the entity of the user who owns this data object
+        return $user === $service->getUser();
+    }
+}
