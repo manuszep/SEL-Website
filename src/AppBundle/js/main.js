c1c4891f69589859d6d4aa351486586e4a8e1a6a
Emmanuel Szép <manuszep@gmail.com>
2016-05-12 11:24:34 +0200

    Add a toggle button on the filter form

--- src/AppBundle/js/main.js
+++ src/AppBundle/js/main.js
@@ -1,7 +1,11 @@
 var $ = require('jquery'),
     Masonry = require('masonry-layout');
 
+import {Filter} from './Filter.js';
+
 var msnry = new Masonry('.grid', {
     itemSelector: 'article',
     columnWidth: 'article'
 });
+
+var filter = new Filter();
