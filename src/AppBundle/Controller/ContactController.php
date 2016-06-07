bbbd56f31ba37de799b10733c047fa19b0a9e0f1
Emmanuel Szép <manuszep@gmail.com>
2016-06-07 13:31:51 +0200

    Handle mail sending

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -17,7 +17,7 @@ class ContactController extends Controller
 {
     /**
      * @Route("/", name="contact_index")
-     * @Method("GET")
+     * @Method({"GET", "POST"})
      */
     public function indexAction(Request $request)
     {
@@ -25,7 +25,38 @@ class ContactController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            
+            $data = $form->getData();
+            $message = \Swift_Message::newInstance()
+                ->setSubject($data['subject'])
+                ->setFrom($data['email'])
+                ->setTo('info@boutsdefisel.be')
+                ->setBody(
+                    $this->renderView(
+                        'Emails/contact.html.twig',
+                        array('name' => $data['name'],
+                              'message' => $data['message']
+                        )
+                    ),
+                    'text/html'
+                )
+                ->addPart(
+                    $this->renderView(
+                        'Emails/contact.txt.twig',
+                        array('name' => $data['name'],
+                              'message' => $data['message']
+                        )
+                    ),
+                    'text/plain'
+                )
+            ;
+            $this->get('mailer')->send($message);
+
+            $this->addFlash(
+                'success',
+                'Votre message a bien été envoyé.'
+            );
+
+            return $this->redirectToRoute('homepage');
         }
 
         return $this->render('contact/index.html.twig', array(
