53e0c222bfa87bc0b5901c7ee3d5b08a64f6f5ed
Emmanuel Szep <manuszep@gmail.com>
2017-03-07 12:31:39 +0100

    fix #15 - Stop click on edit faq from opening the accordion item.

--- src/AppBundle/js/Faq.js
+++ src/AppBundle/js/Faq.js
@@ -5,8 +5,9 @@ export class Faq {
         this._cache = {};
 
         this._cache.list = $('.faq-list');
-        this._cache.questions = this._cache.list.find('.question');
-        this._cache.answers = this._cache.list.find('.answer');
+        this._cache.questions = $('.faq-list .question');
+        this._cache.questions_actions = $('.faq-list .question a');
+        this._cache.answers = $('.faq-list .answer');
 
         this._cache.answers.hide();
 
@@ -15,7 +16,11 @@ export class Faq {
 
     setupEvents() {
         let self = this;
-        this._cache.questions.on('click.selFaq', function() {
+        this._cache.questions_actions.on('click.selFaq', function(e) {
+            e.stopPropagation();
+        });
+
+        this._cache.questions.on('click.selFaq', function(e) {
             self.handleQuestionClick($(this));
         });
     }
