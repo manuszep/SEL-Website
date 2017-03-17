5a824b4516329118bcdd008382e87fd40686a092
Emmanuel Szep <manuszep@gmail.com>
2017-03-17 16:03:06 +0100

    Fix phone number transformer so it works in more cases

--- src/AppBundle/DataTransformer/PhoneNumberTransformer.php
+++ src/AppBundle/DataTransformer/PhoneNumberTransformer.php
@@ -33,13 +33,13 @@ class PhoneNumberTransformer implements DataTransformerInterface
 
         if ($valid_phone_number !== FALSE && $number !== "") {
             if ($valid_phone_number == 'mobile') {
-                $pattern = '/^\s?(\d{4})\s?(\d{2})\s?(\d{2})\s?(\d{2})\s?$/i';
+                $pattern = '/^(\+\d{2})(\d{3})(\d{2})(\d{2})(\d{2})$/i';
             } else {
                 $pattern = '/^(?|(0[2349])\s?(\d{3})|(\d{3})\s?(\d{2}))\s?(\d{2})\s?(\d{2})$/i';
             }
 
             preg_match($pattern, $number, $matches);
-            $number = $matches[1] . ' ' . $matches[2] . ' ' . $matches[3] . ' ' . $matches[4];
+            $number = $matches[1] . ' ' . $matches[2] . ' ' . $matches[3] . ' ' . $matches[4] . ' ' . $matches[5];
         }
 
         return $number;
@@ -51,8 +51,14 @@ class PhoneNumberTransformer implements DataTransformerInterface
             return "";
         }
         $number = preg_replace('/[^0-9+]/', '', $input);
+        $number = preg_replace('/^00/', '+', $number);
+        $number = preg_replace('/^0/', '', $number);
 
-        if (substr($number, 0, 1) != '0' && substr($number, 0, 1) != '+') {
+        if (substr($number, 3, 1) == "0") {
+            $number = substr($number, 0, 3) . substr($number, 4);
+        }
+
+        if (substr($number, 0, 1) != '+') {
             $number = '+32' . $number;
         }
 
